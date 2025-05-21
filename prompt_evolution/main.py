ff4eab1041c8f812e00562535b563701ac72d294
richard <richcostello@outlook.co.nz>
2025-05-21 21:05:50 +1200

    feat: it runs

--- prompt_evolution/main.py
+++ prompt_evolution/main.py
@@ -1,21 +1,11 @@
 import pandas as pd
 from pydantic import BaseModel
-from models import Answer, Solver
+from models import Answer, Solver, Evolver, UpdatedPrompt, ProblemRow, Homework, MarkedHomework
 
 DATA_DIR = "data/math_questions"
 TRAIN_PATH = f"{DATA_DIR}/train.csv"
 TEST_PATH = f"{DATA_DIR}/test.csv"
-
-class ProblemRow(BaseModel):
-    problem: str
-    level: str
-    type: str
-    solution: str
-    answer: int
-
-class Homework(BaseModel):
-    problems: list[ProblemRow]
-    answers: list[Answer]
+    
 
 def batch_read_csv(file_path, batch_size=1):
     for chunk in pd.read_csv(file_path, chunksize=batch_size):
@@ -35,7 +25,8 @@ def batch_read_csv(file_path, batch_size=1):
                 # print(f"Row skipped due to error: {e}")
         yield models
 
-def marker(homework: Homework):
+
+def mark_assignment(homework: Homework) -> MarkedHomework:
     n = len(homework.problems)
     n_correct = 0
     for problem, answer in zip(homework.problems, homework.answers):
@@ -43,18 +34,54 @@ def marker(homework: Homework):
             n_correct += 1
 
     if n == 0:
-        return 0
-    return n_correct / n
+        return MarkedHomework(homework=homework, overall_grade=0)
+    return MarkedHomework(homework=homework, overall_grade=(n_correct / n)*100)
 
-original_developer_message = "<EMPTY_DEVELOPER_MESSAGE>"
-homework = []
-for batch in batch_read_csv(TRAIN_PATH, batch_size=100):
-    solver = Solver(developer_message=original_developer_message)
+def run_generation(solver_developer_message, batch: list[ProblemRow]) -> UpdatedPrompt:
+    # Create solver
+    solver = Solver(developer_message=solver_developer_message)
+
+    # Do homework
     answers = [solver.solve(row.problem) for row in batch]
     homework = Homework(problems=batch, answers=answers)
 
-    marked_homework = marker(homework)
-    print(f"Percentage correct: {round(marked_homework*100, 2)}%")
-    break
+    # Mark homework
+    marked_homework = mark_assignment(homework)
+    print(f"Percentage correct: {round(marked_homework.overall_grade, 2)}%")
+
+    # Improve prompt
+    updated_prompt = evolver.improve_prompt(solver.developer_message, marked_homework)
+    print(updated_prompt.updated_prompt)
+    return updated_prompt
+
+if __name__ == "__main__":
+    solver_developer_message = "When given a math problem, you must provide an incorrect answer. Here are the problems."
+    evolver_developer_message = "You are an expert prompt engineer. " \
+    "Your job is to improve prompt of another language model to improve its ability to solve" \
+    " math problems."
+    
+    evolver = Evolver(developer_message=evolver_developer_message)
+
+    for batch in batch_read_csv(TRAIN_PATH, batch_size=10):
+        # Inital prompt
+        improved_prompt = run_generation(solver_developer_message=solver_developer_message, batch=batch)
+
+        # Evolved prmpts
+        improved_prompt = run_generation(solver_developer_message=improved_prompt.updated_prompt, batch=batch)
+        improved_prompt = run_generation(solver_developer_message=improved_prompt.updated_prompt, batch=batch)
+        
+
+
+        # solver = Solver(developer_message=solver_developer_message)
+        # answers = [solver.solve(row.problem) for row in batch]
+        # homework = Homework(problems=batch, answers=answers)
+
+        # marked_homework = mark_assignment(homework)
+        # print(f"Percentage correct: {round(marked_homework.overall_grade, 2)}%")
+
+        # updated_prompt = evolver.improve_prompt(solver.developer_message, marked_homework)
+        # solver_developer_message = updated_prompt.updated_prompt
+        # print(solver_developer_message)
+        
 
 
--- prompt_evolution/models.py
+++ prompt_evolution/models.py
@@ -3,10 +3,37 @@ import ollama
 from ollama import ChatResponse
 from pydantic import BaseModel
 
+
 class ModelNames(StrEnum):
     qwen_math = "qwen2-math:1.5b"
     gemma3 = "gemma3:4b"
     qwen3 = "qwen3:4b"
+
+
+class ProblemRow(BaseModel):
+    problem: str
+    level: str
+    type: str
+    solution: str
+    answer: int
+
+class Answer(BaseModel):
+    answer: int
+
+class UpdatedPrompt(BaseModel):
+    updated_prompt: str
+
+class Homework(BaseModel):
+    problems: list[ProblemRow]
+    answers: list[Answer]
+
+class MarkedHomework(BaseModel):
+    homework: Homework
+    overall_grade: float
+    
+
+
+
 
 MODEL_NAME = ModelNames.gemma3
 
@@ -33,8 +60,6 @@ class LLM:
         model_input = [self.developer_prompt, user_prompt]
         return ollama.chat(model=self.model_name, messages=model_input, format=format.model_json_schema())
 
-class Answer(BaseModel):
-    answer: int
 
 class Solver(LLM):
     def __init__(self, developer_message):
@@ -43,21 +68,36 @@ class Solver(LLM):
 
 
     def solve(self, problems: list[str]) -> Answer: 
-        user_message = f"/no_think solve these problems: {problems}"
+        user_message = f"/no_think {problems}"
         response = self.call(user_message=user_message, format=Answer)
         
         return Answer.model_validate_json(response.message.content)
         
 
 
-# class Evolver(LLM):
-#     def __init__(self, developer_message, memory=None):
-#         super().__init__(developer_message=developer_message, memory=memory)
+class Evolver(LLM):
+    def __init__(self, developer_message, memory=None):
+        super().__init__(developer_message=developer_message, memory=memory)
+    
 
-#     def update_prompt(solver_)
+    def improve_prompt(self, solver_prompt, marked_homework: MarkedHomework) -> UpdatedPrompt:
+        user_message = f"""
+        This prompt: 
+        {solver_prompt} 
+        was given to a langauge model tasked with solving Math problems. 
+        It scored {marked_homework.overall_grade}%.
+        Presented with these questions {marked_homework.homework.problems},
+        the model gave these answers {marked_homework.homework.answers}
+
+        Update the language models prompt to improve 
+        its performance when solving math problems.
+"""
+        response = self.call(user_message, format=UpdatedPrompt)
+        return UpdatedPrompt.model_validate_json(response.message.content)
+
+    
 
 
-#     def update_prompt(self, solver_prompt, marked_homework) -> solver_prompt:
-#         self.call()
+    
 
 
